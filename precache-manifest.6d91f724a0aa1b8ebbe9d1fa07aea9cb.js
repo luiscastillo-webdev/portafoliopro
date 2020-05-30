8089b6287f97e7a1702fa073bd80d86dc5b6d4bd
cncompusistema <cncompusistema@gmail.com>
2020-05-30 18:37:18 -0500

    Updates

---
+++ precache-manifest.6d91f724a0aa1b8ebbe9d1fa07aea9cb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a6e4eb315a33fb295f26c8635e47a1",
+    "url": "/portafoliopro/index.html"
+  },
+  {
+    "revision": "108853babd045606a525",
+    "url": "/portafoliopro/static/css/2.def00553.chunk.css"
+  },
+  {
+    "revision": "bba03d1edcf35eee5dc7",
+    "url": "/portafoliopro/static/css/main.2f889360.chunk.css"
+  },
+  {
+    "revision": "108853babd045606a525",
+    "url": "/portafoliopro/static/js/2.dc22923f.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portafoliopro/static/js/2.dc22923f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bba03d1edcf35eee5dc7",
+    "url": "/portafoliopro/static/js/main.165facdf.chunk.js"
+  },
+  {
+    "revision": "ecba87a785c17d0a2ce4",
+    "url": "/portafoliopro/static/js/runtime-main.dcec7da3.js"
+  },
+  {
+    "revision": "a9cccfec5cbe883db05545f28f943660",
+    "url": "/portafoliopro/static/media/bg_yo.a9cccfec.jpg"
+  },
+  {
+    "revision": "15e1fcd3eefff16c455cd934807a92ae",
+    "url": "/portafoliopro/static/media/bg_yo2.15e1fcd3.jpg"
+  },
+  {
+    "revision": "9a7d2a549b65f2a2ae9392d28c79e6f6",
+    "url": "/portafoliopro/static/media/derilac_650x350.9a7d2a54.png"
+  },
+  {
+    "revision": "a5b4f4ccf15a9e228c238f72756cd134",
+    "url": "/portafoliopro/static/media/error_404.a5b4f4cc.jpg"
+  },
+  {
+    "revision": "f08e495600d16e1ffd913803de4351f7",
+    "url": "/portafoliopro/static/media/firenet_650x350.f08e4956.png"
+  },
+  {
+    "revision": "364b541b974a24fdb66e2b5fce16d8bf",
+    "url": "/portafoliopro/static/media/leonpaul_650x350.364b541b.png"
+  },
+  {
+    "revision": "38147f6a04a316668367d1ba78bdc41e",
+    "url": "/portafoliopro/static/media/quanthyka_650x350.38147f6a.png"
+  },
+  {
+    "revision": "cc4007e94f8698f8b761d5f53faa7d75",
+    "url": "/portafoliopro/static/media/yo.cc4007e9.png"
+  }
+]);
